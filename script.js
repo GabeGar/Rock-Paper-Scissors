21d624a0859050b5585f67a740e08993d4127b9b
Gabegar <Gagarcia190@gmail.com>
2022-10-07 13:40:16 -0400

    add logic for determining round winner

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a draw."
+    } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
+    playerSelection === 'paper' && computerSelection === 'scissors' || 
+    playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You lose. ${computerSelection} beats ${playerSelection}`
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    }
+}
+
 function getComputerChoice() {
     const RPS = ['Rock', 'Paper', 'Scissors']
     let computerChoice = Math.floor(Math.random() * RPS.length)
     return RPS[computerChoice].toLowerCase()
 }
 
-const computerDecision = getComputerChoice()
+const computerSelection = getComputerChoice()
+console.log(`Computer chose: ${computerSelection}`) // Debugging -- shows comp choice
+const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase()
+console.log(`Player chose: ${playerSelection}`) // Debugging -- show player choice
+
+console.log(playRound(playerSelection, computerSelection)) // Debugging -- output
