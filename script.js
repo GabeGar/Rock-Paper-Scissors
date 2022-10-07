8b360f8c1a1318ab850465993d06ea4fd38141b2
Gabegar <Gagarcia190@gmail.com>
2022-10-07 15:32:12 -0400

    refactored result for draw condition
    This will now be checked in the logic where the score is kept track of.

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
-function getComputerChoice() {
-    const RPS = ['rock', 'paper', 'scissors']
-    let computerChoice = Math.floor(Math.random() * RPS.length)
-    return RPS[computerChoice]
-}
-
-function playRound(playerSelection, computerSelection) {
-    console.log(`Player chose: ${playerSelection}\nComputer chose: ${computerSelection}`)
-
-    if (playerSelection === computerSelection) {
-        return "It's a draw."
-    } else if (
-        playerSelection === 'rock' && computerSelection === 'paper' ||
-        playerSelection === 'paper' && computerSelection === 'scissors' || 
-        playerSelection === 'scissors' && computerSelection === 'rock') {
-        alert("You lost the round.")
-        return false
-    } else if (
-        computerSelection === 'rock' && playerSelection === 'paper' ||
-        computerSelection === 'paper' && playerSelection === 'scissors' || 
-        computerSelection === 'scissors' && playerSelection === 'rock') {
-        alert("You Won the round!")
-        return true
-    } else {
-        return 'Invalid input. Please choose "rock", "paper" or "scissors", ONLY.'
-    }
-}
-
 function game() {
+
+    function getComputerChoice() {
+        const RPS = ['rock', 'paper', 'scissors']
+        let computerChoice = Math.floor(Math.random() * RPS.length)
+        return RPS[computerChoice]
+    }
+
+    function playRound(playerSelection, computerSelection) {
+        console.log(`Player chose: ${playerSelection}\nComputer chose: ${computerSelection}`)
+
+        if (playerSelection === computerSelection) {
+            return
+        } else if (
+            playerSelection === 'rock' && computerSelection === 'paper' ||
+            playerSelection === 'paper' && computerSelection === 'scissors' || 
+            playerSelection === 'scissors' && computerSelection === 'rock') {
+            return false
+        } else if (
+            computerSelection === 'rock' && playerSelection === 'paper' ||
+            computerSelection === 'paper' && playerSelection === 'scissors' || 
+            computerSelection === 'scissors' && playerSelection === 'rock') {
+            return true
+        } else {
+            return 'Invalid input. Please choose "rock", "paper" or "scissors", ONLY.'
+        }
+    }
+
+
     let computerScore = 0;
     let playerScore = 0;
 
-    const computerSelection = getComputerChoice()
-    const playerSelection = prompt("rock, paper or scissors?").toLowerCase()
+    for (let i = 0; i < 5; i++){
+        const computerSelection = getComputerChoice()
+        const playerSelection = prompt("rock, paper or scissors?").toLowerCase()
 
-    let didPlayerScore = playRound(playerSelection, computerSelection) // Output
+        let didPlayerScore = playRound(playerSelection, computerSelection) // Output
 
-    if (didPlayerScore === false) {
-        computerScore += 1
-    } else {
-        playerScore += 1
+        if (didPlayerScore === false) {
+            alert("Round lost. Computer scored.")
+            computerScore += 1
+        } else if (didPlayerScore === true) {
+            alert("Round won. You scored.")
+            playerScore += 1
+        } else {
+            alert("Draw")
+        }
+
+        console.log(playerScore)
+        console.log(computerScore)
     }
-    console.log(playerScore)
-    console.log(computerScore)
 }
+
 game()
