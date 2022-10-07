3b72de592a8d32b8bae5eb0f473bf8944d4d5edd
Gabegar <Gagarcia190@gmail.com>
2022-10-07 13:22:47 -0400

    adjust computer choice to return lowercase string

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     const RPS = ['Rock', 'Paper', 'Scissors']
     let computerChoice = Math.floor(Math.random() * RPS.length)
-    return RPS[computerChoice]
+    return RPS[computerChoice].toLowerCase()
 }
 
 const computerDecision = getComputerChoice()
