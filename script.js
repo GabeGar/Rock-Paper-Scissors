c5a254ab4b1992fa640436c7f44f7a131904afed
Gabegar <Gagarcia190@gmail.com>
2022-10-07 15:58:07 -0400

    added two new functions
    1. checkAndUpdateScore, will update scores; uses
    a boolean based on player scoring or lack-thereof,
    to validate and update computer + player scores, accordingly.
    
    2. checkForWinner, will pull player and computer scores, as inputs,
    to compare against each other. Announces in console the game winner,
    if there is one, or a draw, otherwise.

--- script.js
+++ script.js
@@ -26,29 +26,56 @@ function game() {
         }
     }
 
+    function checkWinner(playerScore, computerScore) {
+        console.clear() // Clear console prior to displaying final score
+        console.log(`Player final score: ${playerScore}`)
+        console.log(`Computer final score: ${computerScore}`)
+
+        if (playerScore === computerScore) {
+            console.log("Well would you look at that? It's a draw game.")
+        } else if (playerScore < computerScore) {
+            console.log("Sorry player. Computer has won the game.")
+        } else {
+            console.log("Player has won the game! Congratulations!")
+        }
+    }
+
 
     let computerScore = 0;
     let playerScore = 0;
 
-    for (let i = 0; i < 5; i++){
+    console.log(`Players current score: ${playerScore}`)
+    console.log(`Computers current score: ${computerScore}`)
+
+    for (let i = 0; i < 5; i++) {
+
         const computerSelection = getComputerChoice()
         const playerSelection = prompt("rock, paper or scissors?").toLowerCase()
 
         let didPlayerScore = playRound(playerSelection, computerSelection) // Output
 
-        if (didPlayerScore === false) {
-            alert("Round lost. Computer scored.")
-            computerScore += 1
-        } else if (didPlayerScore === true) {
-            alert("Round won. You scored.")
-            playerScore += 1
-        } else {
-            alert("Draw")
+        function checkAndUpdateScore(validate) {
+            if (didPlayerScore === false) {
+                alert("Round lost. Computer scored.")
+                computerScore += 1
+            } else if (didPlayerScore === true) {
+                alert("Round won! You scored.")
+                playerScore += 1
+            } else {
+                alert("Round Draw")
+            }
         }
 
-        console.log(playerScore)
-        console.log(computerScore)
+        checkAndUpdateScore(didPlayerScore)
+        console.log(`Players current score: ${playerScore}`)
+        console.log(`Computers current score: ${computerScore}`)
     }
+
+    checkWinner(playerScore, computerScore)
 }
 
 game()
+
+
+// BUG: Player can choose anything other than string and it will be process by program.
+//      Need to catch edge case for anything other than strings, when prompting for player input.
