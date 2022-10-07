cdc916035e1c0bf85b4541622aa30bac0f7a027f
Gabegar <Gagarcia190@gmail.com>
2022-10-07 14:56:55 -0400

    added game function to enclose game code

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
+function getComputerChoice() {
+    const RPS = ['rock', 'paper', 'scissors']
+    let computerChoice = Math.floor(Math.random() * RPS.length)
+    return RPS[computerChoice]
+}
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a draw."
-    } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
-    playerSelection === 'paper' && computerSelection === 'scissors' || 
-    playerSelection === 'scissors' && computerSelection === 'rock') {
-        return `You lose. ${computerSelection} beats ${playerSelection}`
+    } else if (
+        playerSelection === 'rock' && computerSelection === 'paper' ||
+        playerSelection === 'paper' && computerSelection === 'scissors' || 
+        playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You lose. Computer chose: ${computerSelection} which beats player choice: ${playerSelection}`
+    } else if (
+        computerSelection === 'rock' && playerSelection === 'paper' ||
+        computerSelection === 'paper' && playerSelection === 'scissors' || 
+        computerSelection === 'scissors' && playerSelection === 'rock') {
+        return `You Win! You chose: ${playerSelection}, which beats computer choice: ${computerSelection}`
     } else {
-        return `You win! ${playerSelection} beats ${computerSelection}`
+        return 'Invalid input. Please choose "rock", "paper" or "scissors", ONLY.'
     }
 }
 
-function getComputerChoice() {
-    const RPS = ['Rock', 'Paper', 'Scissors']
-    let computerChoice = Math.floor(Math.random() * RPS.length)
-    return RPS[computerChoice].toLowerCase()
-}
+function game() {
 
 const computerSelection = getComputerChoice()
-console.log(`Computer chose: ${computerSelection}`) // Debugging -- shows comp choice
-const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase()
-console.log(`Player chose: ${playerSelection}`) // Debugging -- show player choice
+const playerSelection = playerChoice = prompt("rock, paper or scissors?").toLowerCase()
+console.log(playRound(playerSelection, computerSelection)) // Output
+}
 
-console.log(playRound(playerSelection, computerSelection)) // Debugging -- output
+game()
