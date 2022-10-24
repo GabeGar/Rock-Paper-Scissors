87c6f77cc409283e072a2e291657ece133a7d6a2
Gabegar <Gagarcia190@gmail.com>
2022-10-24 12:30:07 -0400

    updated display func to incorporate new images
    Images were created via the doc.createElement methods. Attributes were
    assigned to properly display the images. display func was then updated
    to append and remove the images, based on the choices from both the
    player and the computer.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function game() {
+    // Query Selectors
     const allButtons = document.querySelectorAll("#btn");
     const resultsContainer = document.querySelector(".results-container");
     const playerScoreDiv = document.querySelector("#player");
@@ -10,7 +11,31 @@ function game() {
     //New element(s)
     const announceWinner = document.createElement("div");
 
-    // Styles
+    // Images
+    const playerRockImage = document.createElement("img");
+    const playerPaperImage = document.createElement("img");
+    const playerScissorsImage = document.createElement("img");
+    const computerRockImage = document.createElement("img");
+    const computerPaperImage = document.createElement("img");
+    const computerScissorsImage = document.createElement("img");
+
+    // Player image attributes
+    playerRockImage.src = "images/rock.png";
+    playerRockImage.setAttribute("style", "width: 200px; height: 200px;");
+    playerPaperImage.src = "images/paper.png";
+    playerPaperImage.setAttribute("style", "width: 200px; height: 200px;");
+    playerScissorsImage.src = "images/scissors.png";
+    playerScissorsImage.setAttribute("style", "width: 200px; height: 200px;");
+
+    // Computer image attributes
+    computerRockImage.src = "images/rock.png";
+    computerRockImage.setAttribute("style", "width: 200px; height: 200px;");
+    computerPaperImage.src = "images/paper.png";
+    computerPaperImage.setAttribute("style", "width: 200px; height: 200px;");
+    computerScissorsImage.src = "images/scissors.png";
+    computerScissorsImage.setAttribute("style", "width: 200px; height: 200px;");
+
+    // Styles & Attributes
     roundStatus.setAttribute("style", "font-size: 24px; font-weight: bold");
     announceWinner.setAttribute("style", "font-size: 24px; font-weight: bold");
 
@@ -79,17 +104,46 @@ function game() {
         } else return false;
     }
 
-    function displayChosen(player, computer) {
-        playerChose.textContent = `Player Chose: ${player.toUpperCase()}`;
-        computerChose.textContent = `Computer Chose: ${computer.toUpperCase()}`;
+    function displayChosenChoices(playerChoice, computerChoice) {
+        // Player
+        playerChose.appendChild(playerRockImage);
+        playerChose.appendChild(playerPaperImage);
+        playerChose.appendChild(playerScissorsImage);
+
+        if (playerChoice === "rock") {
+            playerChose.removeChild(playerPaperImage);
+            playerChose.removeChild(playerScissorsImage);
+        } else if (playerChoice === "paper") {
+            playerChose.removeChild(playerRockImage);
+            playerChose.removeChild(playerScissorsImage);
+        } else if (playerChoice === "scissors") {
+            playerChose.removeChild(playerRockImage);
+            playerChose.removeChild(playerPaperImage);
+        }
+
+        // Computer
+        computerChose.appendChild(computerRockImage);
+        computerChose.appendChild(computerPaperImage);
+        computerChose.appendChild(computerScissorsImage);
+
+        if (computerChoice === "rock") {
+            computerChose.removeChild(computerPaperImage);
+            computerChose.removeChild(computerScissorsImage);
+        } else if (computerChoice === "paper") {
+            computerChose.removeChild(computerRockImage);
+            computerChose.removeChild(computerScissorsImage);
+        } else if (computerChoice === "scissors") {
+            computerChose.removeChild(computerRockImage);
+            computerChose.removeChild(computerPaperImage);
+        }
     }
 
     //Main game function
     function playRound(playerEvent) {
-        const playerSelection = playerEvent.target.textContent.toLowerCase();
         const compSelection = getComputerChoice();
+        const playerSelection = playerEvent.target.textContent.toLowerCase();
 
-        displayChosen(playerSelection, compSelection);
+        displayChosenChoices(playerSelection, compSelection);
 
         let status = getRoundStatus(playerSelection, compSelection);
         updateScore(status);
